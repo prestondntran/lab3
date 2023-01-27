55ccd929de7963f8d814bf945367914e924ed9e5
prestondntran <prestondnt@gmail.com>
2023-01-26 16:54:18 -0800

    lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = {8, 12, 16, 20, 24};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 24, 20, 16, 12, 8 }, input2);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = { 5, 10, 15 };
+    assertArrayEquals(new int[]{ 15, 10, 5 }, ArrayExamples.reversed(input2));
   }
 }
